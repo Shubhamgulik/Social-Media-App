cc5171efd00eb87fa39a501208db8b207c70cc86
Shubhamgulik <42814032+Shubhamgulik@users.noreply.github.com>
2023-01-10 15:19:16 +0530

    Added Delete POST module

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const Comment = require('../models/comment')
 module.exports.create = function(req,res){
 
     Post.create({
@@ -10,5 +10,31 @@ module.exports.create = function(req,res){
 
         console.log(post);
         return res.redirect('back');
+    })
+}
+
+
+// Function to delete post which receive post_id as parameter
+module.exports.destroy = function(req,res){
+
+    console.log("Deleting post : ",req.params.id);
+    //If post exists then only we can delete
+    Post.findById(req.params.id, function(err,post){
+        if(err){console.log("Not able to find post: ",err);}
+        if(post){
+            // We'll check if the user is authorised to delete post
+            if(post.user == req.user.id){
+                post.remove();
+
+                Comment.deleteMany({'post' : req.params.id}, function(err){
+                    if(err){console.log("Error in finding posts");}
+
+                    return res.redirect('back');
+                });
+            }
+
+        }else{
+            return res.redirect('back');
+        }
     })
 }
